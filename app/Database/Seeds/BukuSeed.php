1a248499d60ae95b79e68102447e87f02ce445f0
halimkun <ffaisalhalim@gmail.com>
2021-12-22 00:00:34 +0700

    edit seeder

--- app/Database/Seeds/BukuSeed.php
+++ app/Database/Seeds/BukuSeed.php
@@ -10,6 +10,39 @@ class BukuSeed extends Seeder
     {
         $data = [
             [
+                'idk'           => 857349,
+                'k'             => 'teknologi',
+                'created_at'    => '2021-12-18 02:15:07',
+                'updated_at'    => '2021-12-18 02:15:07',
+                'deleted_at'    => NULL
+            ],
+            [
+                'idk'           => 857350,
+                'k'             => 'sejarah',
+                'created_at'    => '2021-12-18 02:15:18',
+                'updated_at'    => '2021-12-18 02:15:18',
+                'deleted_at'    => NULL
+            ],
+            [
+                'idk'           => 857351,
+                'k'             => 'sains',
+                'created_at'    => '2021-12-18 02:15:22',
+                'updated_at'    => '2021-12-18 02:15:22',
+                'deleted_at'    => NULL
+            ],
+            [
+                'idk'           => 857352,
+                'k'             => 'matematika',
+                'created_at'    => '2021-12-18 02:15:22',
+                'updated_at'    => '2021-12-18 02:15:22',
+                'deleted_at'    => NULL
+            ],
+        ];
+        $this->db->table('kategori')->insertBatch($data);
+
+
+        $data = [
+            [
                 'id_buku'       => 4729423,
                 'kode_buku'     => 'MLB61bd99fc7dbe8',
                 'stok_buku'     => 7,
@@ -160,7 +193,6 @@ class BukuSeed extends Seeder
                 'deleted_at'    => NULL
             ]
         ];
-
         $this->db->table('buku')->insertBatch($data);
     }
 }
