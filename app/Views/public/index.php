f22884fe3c6070d6565695d23f3171345406e143
halimkun <ffaisalhalim@gmail.com>
2021-12-27 22:09:32 +0700

    fix public title

--- app/Views/public/index.php
+++ app/Views/public/index.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Blank Page &mdash; Stisla</title>
+    <title><?= ($title != null) ? $title : "Home" ?></title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="/assets/modules/bootstrap/css/bootstrap.min.css">
@@ -27,13 +27,24 @@
     <!-- JavaScript -->
     <script src="/assets/modules/jquery.min.js"></script>
 
+    <style>
+        .sep::before {
+            content: ' ';
+            border-radius: 5px;
+            height: 5px;
+            width: 80px;
+            background-color: #6777ef;
+            display: inline-block;
+        }
+    </style>
+
 </head>
 
 <body class="layout-3">
     <div style="height: 100vh">
         <nav class="navbar fixed-top navbar-light navbar-expand-lg main-navbar">
             <a href="/" class="navbar-brand sidebar-gone-hide text-dark">Stisla</a>
-            <a href="#" class="nav-link sidebar-gone-show" data-toggle="sidebar"><i class="fas fa-bars" style="margin-top: 21px;"></i></a>
+            <a href="#" class="nav-link sidebar-gone-show" data-toggle="sidebar"><i class="fas fa-bars bg-primary" style="margin-top: 10px; padding: 10px; border-radius: 5px;"></i></a>
             <div class="nav-collapse d-none d-md-block d-lg-block">
                 <a class="sidebar-gone-show nav-collapse-toggle nav-link" href="#">
                     <i class="fas fa-ellipsis-v"></i>
@@ -56,7 +67,6 @@
                         <?php endif ?>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right">
-                        <div class="dropdown-title">Logged in 5 min ago</div>
                         <?php if (logged_in()) : ?>
                             <a href="/user" class="dropdown-item has-icon">
                                 <i class="far fa-user"></i> Profile
@@ -119,8 +129,8 @@
 
         <div class="d-sm-flex align-items-center justify-content-between w-100 h-100" <?= ($uagent->isMobile()) ? 'style="margin-top: 100px;"' : '' ?>>
             <div class="col-md-4 mx-auto mb-4 mb-sm-0 headline">
-                <h1 class="display-4 my-4 font-weight-bold">Enter Your <span style="color: #9B5DE5;">Headline Here</span></h1>
-                <a href="#" class="btn px-5 py-3 text-white mt-3 mt-sm-0" style="border-radius: 30px; background-color: #9B5DE5;">Get Started</a>
+                <h1 class="display-4 my-4 font-weight-bold"><span class="text-primary">Welcome To</span> PERPUS<span class="text-primary">ID</span></h1>
+                <a href="#" class="btn px-5 py-3 text-white mt-3 mt-sm-0 btn-primary" style="border-radius: 30px;">Get Started</a>
             </div>
             <!-- in mobile remove the clippath -->
             <div class="col-md-8 h-100 clipped" style="min-height: 350px; background-image: url(https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80); background-position: center; background-size: cover;">
@@ -128,6 +138,119 @@
             </div>
         </div>
     </div>
+
+    <section class="section-body p-5 bg-light">
+        <div class="row py-5">
+            <div class="col-md-4 col-sm-12 col-xs 12 text-center">
+                <div class="display-4 counternya"><?= count($cbuku) ?></div>
+                <h5 class="font-weight-light">Buku</h5>
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs 12 text-center">
+                <div class="display-4 counternya"><?= count($cuser) ?></div>
+                <h5 class="font-weight-light">User</h5>
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs 12 text-center">
+                <div class="display-4 counternya"><?= count($cpeminjaman) ?></div>
+                <h5 class="font-weight-light">Pinjaman</h5>
+            </div>
+        </div>
+    </section>
+
+    <section class="p-5 section-body">
+        <div class="text-center mb-4">
+            <h1 class="font-weight-light">Our Collection</h1>
+            <div class="sep"></div>
+        </div>
+        <div class="row p-5">
+            <?php foreach ($buku as $buku) : ?>
+                <div class="col-md-6 col-sm-12 col-xs-12 ">
+                    <div class="card rounded bg-light shadow-sm card-large-icons">
+                        <div class="card-icon bg-primary text-white " data-image="/assets/imgs/cover/<?= $buku['cover_buku'] ?>" data-title="<?= $buku['judul_buku'] ?>" style="background-image: url('/assets/imgs/cover/<?= $buku['cover_buku'] ?>'); background-size: cover; background-position: center; background-repeat: no-repeat; background-clip: border-box; align-items: flex-start !important; justify-content: flex-end!important">
+                            <kbd class="bg-danger shadow" style="border-radius: 0px 0px 0px 10px;"><?= $buku['kategori'] ?></kbd>
+                        </div>
+                        <div class="card-body">
+                            <h4><?= $buku['judul_buku'] ?></h4>
+                            <p>
+                                <strong>Penulis : </strong><?= $buku['penulis_buku'] ?> <br>
+                                <strong>Penerbit : </strong><?= $buku['penerbit_buku'] ?> <br>
+                                <strong>Tahun Terbit : </strong><?= $buku['tahun_terbit'] ?>
+                            </p>
+                            <button class="btn btn-sm btn-primary" id="<?= $buku['kode_buku'] ?>">Detail<i class="fas fa-chevron-right pl-2 card-cta"></i></button>
+                        </div>
+                    </div>
+                </div>
+                <!-- ----------------------------------------- -->
+                <!-- Modal <?= $buku['judul_buku'] ?> -->
+                <?php $img = $buku['cover_buku'] ?>
+                <div class="modal-part" id="<?= md5(md5(md5(md5($buku['kode_buku'])))) ?>">
+                    <div class="row">
+                        <div class="col-md-5 mb-3 d-flex justify-content-center align-items-center">
+                            <img src="/assets/imgs/cover/<?= $buku['cover_buku'] ?>" alt="" class="img-fluid w-75">
+                        </div>
+                        <div class="col-md-7 mb-3">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Judul</th>
+                                        <td><?= $buku['judul_buku'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Kategori</th>
+                                        <td><?= $buku['kategori'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Penulis</th>
+                                        <td><?= $buku['penulis_buku'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Penerbit</th>
+                                        <td><?= $buku['penerbit_buku'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Tahun Terbit</th>
+                                        <td><?= $buku['tahun_terbit'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Stok Buku</th>
+                                        <td><?= $buku['stok_buku'] ?></td>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                    <blockquote><strong>Sinopsis</strong> ~ <?= $buku['sinopsis'] ?></blockquote>
+                    <div class="d-flex justify-content-end">
+                        <a href="/u/books/req" class="btn btn-primary"><i class="far fa-bookmark mr-2"></i>Pinjam Buku</a>
+                    </div>
+                </div>
+                <script>
+                    $(document).ready(function() {
+                        $("#<?= $buku['kode_buku'] ?>").fireModal({
+                            size: 'modal-lg',
+                            title: "<?= $buku['judul_buku'] ?>",
+                            body: $("#<?= md5(md5(md5(md5($buku['kode_buku'])))) ?>"),
+                            center: true
+                        })
+                    });
+                </script>
+                <!-- End Modal <?= $buku['judul_buku'] ?> -->
+            <?php endforeach ?>
+        </div>
+    </section>
+
+    <script>
+        $('.counternya').each(function() {
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 5000,
+                easing: 'swing',
+                step: function(now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        });
+    </script>
 
     <!-- General JS Scripts -->
     <!-- <script src="/assets/modules/popper.js"></script> -->
