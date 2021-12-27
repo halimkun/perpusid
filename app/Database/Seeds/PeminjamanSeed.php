20e66a1b63871351781425ab05bd06c8e6846a8d
halimkun <ffaisalhalim@gmail.com>
2021-12-27 22:13:23 +0700

    dynamic peminjaman & user seed

---
+++ app/Database/Seeds/PeminjamanSeed.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use App\Models\Peminjaman;
+use CodeIgniter\Database\Seeder;
+use CodeIgniter\I18n\Time;
+
+class PeminjamanSeed extends Seeder
+{
+    public function run()
+    {
+        // Database initialize
+        $db = \Config\Database::connect();
+
+        // User model
+        $user = $db->table('users');
+        $user->select('id');
+        $usid = $user->get();
+
+        // Buku model
+        $buku = $db->table('buku');
+        $buku->select('kode_buku');
+        $buk = $buku->get();
+
+        // Time
+        $now = new Time('now', 'Asia/Jakarta');
+
+        // faker initialize
+        $peminjam = new Peminjaman();
+        $faker    = \Faker\Factory::create('id_ID');
+
+        for ($i = 1; $i < 27; $i++) {
+            $tgl_pinjam  = new Time("2021-12-$i", 'Asia/Jakarta');
+            $tgl_kembali = $tgl_pinjam->addDays(7);
+            
+            for ($j = 0; $j < rand(1, 9); $j++) {
+                $uid = $faker->randomElement($usid->getResult('array'));
+                $bid = $faker->randomElement($buk->getResultArray());
+    
+                $peminjam->save([
+                    'kode_peminjaman'   => uniqid(),
+                    'kode_buku'         => $bid,
+                    'userid'            => $uid,
+                    'tanggal_pinjam'    => $tgl_pinjam->toDateString(),
+                    'tanggal_kembali'   => $tgl_kembali->toDateString(),
+                    'peminjaman_status' => ($tgl_kembali < $now) ? 1 : $faker->randomElement([0, 2, 3])
+                ]);
+            }
+        }
+
+
+        /** status peminjaman
+         * 1 dikembalikan
+         * 2 dipinjamkan
+         * 3 proses
+         * 0 ditolak 
+         **/
+    }
+}
--- app/Database/Seeds/UserSeed.php
+++ app/Database/Seeds/UserSeed.php
@@ -4,67 +4,52 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 use App\Entities\User as EntitiesUser;
+use App\Models\UserModel;
 
 class UserSeed extends Seeder
 {
     public function run()
     {
         // User Data
-        $users = [
-            [
-                'id'            => 64284,
-                'firstname'     => "ahmed",
-                'lastname'      => "nagi",
-                'email'         => "user1@gmail.com",
-                'username'      => "user1",
-                'tgl_lahir'     => "2001-10-04",
-                'phone'         => "082375849284",
-                'gender'        => "L",
-                'profile'       => "avatar.png",
-                'address'       => "Alamatnya diutara sana",
-                'password_hash' => '$2y$10$0jKJoWYAdVtlhMoHos.WBeF5pDz.Zhq6snvAJIUXy5WQFq8gZFC1W',
-                'active'        => 1,
-                'reset_hash'    => null,
-                'reset_at'      => null,
-                'reset_expires' => null,
-                // 'password'      => "123456789",
-            ],
-            [
-                'id'            => 64285,
-                'firstname'     => "Abhishek",
-                'lastname'      => "Bagul",
-                'email'         => "user2@gmail.com",
-                'username'      => "user2",
-                'tgl_lahir'     => "2001-10-10",
-                'phone'         => "082375849344",
-                'gender'        => "L",
-                'profile'       => "avatar.png",
-                'address'       => "Alamatnya diselatan jauh sana",
-                'password_hash' => '$2y$10$0jKJoWYAdVtlhMoHos.WBeF5pDz.Zhq6snvAJIUXy5WQFq8gZFC1W',
-                'active'        => 1,
-                'reset_hash'    => null,
-                'reset_at'      => null,
-                'reset_expires' => null,
-                // 'password'      => "123456789",
-            ],
-        ];
-        // $data = new EntitiesUser($users);
-        // dd($data);
+        $user = new UserModel();
+        $faker = \Faker\Factory::create('id_ID');
 
-        $this->db->table('users')->insertBatch($users);
-        // -------------------------------------------------------- //
+        for ($i = 2; $i < 50; $i++) {
+            $gender = $faker->randomElement(['male', 'female']);
+            $jk     = $faker->randomElement(['L', 'P']);
+            $join   = $faker->dateTimeThisYear();
+
+            $user->withGroup('anggota')->insert([
+                'id'            => $i,
+                'email'         => "user$i@gmail.com",
+                'username'      => "user$i",
+                'firstname'     => $faker->firstName($gender),
+                'lastname'      => $faker->lastName(),
+                'tgl_lahir'     => $faker->date('Y-m-d'),
+                'phone'         => $faker->phoneNumber(),
+                'gender'        => $jk,
+                'profile'       => "avatar.png",
+                'address'       => $faker->address(),
+                'password_hash' => '$2y$10$0jKJoWYAdVtlhMoHos.WBeF5pDz.Zhq6snvAJIUXy5WQFq8gZFC1W',
+                'active'        => 1,
+                'created_at'    => $join,
+                'updated_at'    => $join,
+                'deleted_at'    => NULL
+            ]);
+            // 'password'      => "123456789",
+        }
 
         // auth_groups_users Data
-        $auth_groups_users = [
-            [
-                "group_id" => 49329,
-                "user_id"  => 64284
-            ],
-            [
-                "group_id" => 49329,
-                "user_id"  => 64285
-            ],
-        ];
-        $this->db->table('auth_groups_users')->insertBatch($auth_groups_users);
+        // $auth_groups_users = [
+        //     [
+        //         "group_id" => 49329,
+        //         "user_id"  => 64284
+        //     ],
+        //     [
+        //         "group_id" => 49329,
+        //         "user_id"  => 64285
+        //     ],
+        // ];
+        // $this->db->table('auth_groups_users')->insertBatch($auth_groups_users);
     }
 }
